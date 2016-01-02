e118eb2456339288dc7211a4c7e736c509286add
Maksim Kostromin <daggerok@gmail.com>
2016-01-02 08:05:25 +0200

    add default redirect ctrl

--- src/main/java/com/daggerok/cloud/spring/config/Application.java
+++ src/main/java/com/daggerok/cloud/spring/config/Application.java
@@ -3,11 +3,20 @@ package com.daggerok.cloud.spring.config;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @EnableConfigServer
 @SpringBootApplication
 public class Application {
-	public static void main(String[] args) {
-		SpringApplication.run(Application.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
+
+@Controller
+class Index {
+    @RequestMapping("/") public String redirect() {
+        return "redirect:/default/master";
+    }
 }
